1bf2283aa330db8ba23c38b361210b9170a0c407
PhilipK-webdev <philipk179@gmail.com>
2024-01-16 08:06:34 +0200

    addign redux-toolkit to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../src/features/counter/counterSlice.js";
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -13,31 +17,45 @@ function App() {
         console.error("Error fetching categories:", error);
       }
     };
-    const fetchDataNet = async () => {
-      try {
-        const response = await fetch("/api/v2/details", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ name: "John", age: 30 }),
-        });
-        console.log("responsev2", response);
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-      }
-    };
+    // const fetchDataNet = async () => {
+    //   try {
+    //     const response = await fetch("/api/v2/details", {
+    //       method: "POST",
+    //       headers: {
+    //         Accept: "application/json",
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify({ name: "John", age: 30 }),
+    //     });
+    //     console.log("responsev2", response);
+    //     const data = await response.json();
+    //     console.log(data);
+    //   } catch (error) {
+    //     console.error("Error fetching categories:", error);
+    //   }
+    // };
 
     fetchData();
-    fetchDataNet();
+    // fetchDataNet();
   }, []);
   return (
-    <>
-      <div>HELLO</div>
-    </>
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
   );
 }
 
